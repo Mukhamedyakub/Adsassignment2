4f457a89a40384bddbdad28ef9259a7fd228079b
Mukhamedyakub <230198@astanait.edu.kz>
2024-04-26 14:49:55 +0500

    created MyArrayMinHeap

--- MyArrayMinHeap.java
+++ MyArrayMinHeap.java
@@ -40,3 +40,40 @@ public class MyArrayMinHeap<T extends Comparable<T>> {
      * return the smallest element of the heap
      * throws NoSuchElementException if the heap is empty
      */
+    public T getMin() {
+        if (empty()) throw new NoSuchElementException("Heap is empty"); // Check if empty before accessing
+        return elements.get(0); // The root element is always at index 0
+    }
+
+    /**
+     * Inserts a new item into the heap.
+     *
+     * item the item to be added to the heap
+     */
+    public void insert(T item) {
+        elements.add(item); // Add the item at the end of the list
+        traverseUp(elements.size() - 1); // Restore the heap order from the added item upwards
+    }
+
+    /**
+     * Removes and returns the minimum element from the heap.
+     *
+     * return the removed smallest element
+     * throws NoSuchElementException if the heap is empty
+     */
+    public T extractMin() {
+        if (empty()) throw new NoSuchElementException("Heap is empty"); // Ensure the heap is not empty
+        T min = elements.get(0); // The root element, which is the smallest
+        T lastItem = elements.remove(elements.size() - 1); // Remove the last item
+        if (!empty()) {
+            elements.set(0, lastItem); // Replace the root with the last item
+            heapify(0); // Restore heap order from the root down
+        }
+        return min;
+    }
+
+    /**
+     * Restores the heap property starting from a given index and moving downwards.
+     *
+     * index the starting index to begin heapify
+     */
